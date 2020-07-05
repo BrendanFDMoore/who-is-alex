44ec617a7575bcffaeb329eb6122314840899e28
Brendan Moore <git@bfdmoore.com>
2020-07-05 04:04:23 -0400

    Add Netlify functions with auto-rebuilding

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,34 +1,77 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+// import { Link } from "gatsby"
+// import PropTypes from "prop-types"
+import React, {useCallback, useState} from "react"
 
-import { Container, Button, TextInput } from "nes-react"
+import { Button, TextInput } from "nes-react"
 
-const Header = ({ intro = null, siteTitle = 'Set [siteTitle]' }) => (
-  <>
-    <h1>Welcome!</h1>
-    <p>This is a work in progress.</p>
-    <div>
-      <TextInput
-        label="Join a game"
-        placeholder="ABCD"
-        value={null}
-        // onChange={e => this.setState({ textInput: e.target.value })}
-        onChange={e => ({})}
-      />
-      <Button primary>Join Game</Button>
-      <h3>OR</h3>
-      <Button success>Create New Game</Button>
-    </div>
-  </>
-)
+const Welcome = () => {
+  const [gamecode, setGamecode] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+  const createGame = useCallback(async () => {
+    console.log('createGame callback')
+    setErrorMessage(null)
+    const response = await fetch("/.netlify/functions/create-game",{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({creator: 'me', coolvalue:456}),
+    });
+    console.log({response});
+    if (response.status >= 400) {
+      console.log('error status')
+      setErrorMessage('Encountered error creating game. Please try again.')
+    } else {
+      const data = await response.json();
+      console.log(data)
+    }
+  }, []) 
+
+  const joinGame = useCallback(async () => {
+    console.log('joinGame callback')
+    setErrorMessage(null)
+    const response = await fetch("/.netlify/functions/join-game",{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({code: gamecode}),
+    });
+    const games = await response.json();
+    console.log(games);
+    if (response.status >= 400) {
+      setErrorMessage('Invalid code');
+    } else {
+      // redirect to games[0].uuid
+    }
+  }, [gamecode]) 
+
+
+  return (
+    <>
+      <h1>Welcome!</h1>
+      <p>This is a work in progress.</p>
+      <div>
+        <TextInput
+          label="Join a game"
+          placeholder="e.g. ABCD"
+          value={gamecode}
+          onChange={e => setGamecode(e.target.value.toUpperCase())}
+        />
+        <Button primary onClick={joinGame}>Join Game</Button>
+        <h3>OR</h3>
+        <Button success onClick={createGame}>Create New Game</Button>
+        <p>{errorMessage}</p>
+
+      </div>
+    </>
+  );
+}
+Welcome.propTypes = {
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
+Welcome.defaultProps = {
 }
 
-export default Header
+export default Welcome
