d28cd8ee6f4c111b6506ab21600b5699a7f38735
Brendan Moore <git@bfdmoore.com>
2021-10-31 02:07:24 -0400

    WIP - get room peers?

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,15 +1,47 @@
-import React, {useState, useEffect} from "react"
+import React, {useState, useEffect, useCallback} from "react"
 import { Link } from "gatsby"
 import querystring from "querystring"
+import Peer from 'peerjs';
+
+import { Button, Container, TextInput } from "nes-react"
 
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
 
-const GamePage = ({location}) => {
-  const [uuid, setUuid] = useState('INITIAL_UUID');
 
-  useEffect(() => {
-    function setGameUuidFromQuery() {
+const NicknameInput = ({setNickname}) => {
+  const [internalNickname, setInternalNickname] = useState('');
+
+  const submitNickname = useCallback(async () => {
+    console.log('submitNickname callback')
+    setNickname(internalNickname)
+  }, [internalNickname, setNickname]);
+
+  return (
+    <div>
+      <TextInput
+        label="Enter a nickname..."
+        placeholder="e.g. Sam"
+        value={internalNickname}
+        onChange={e => setInternalNickname(e.target.value)}
+      />
+      <Button primary onClick={submitNickname} disabled={internalNickname.length == 0}>⏎</Button>
+    </div>
+  )
+}
+
+const GamePage = ({location}) => {
+  const [uuid, setUuid] = useState(null);
+  const [peerId, setPeerId] = useState(null);
+  const [nickname, setNickname] = useState('');
+
+  // const c = useCallback(async () => {
+  //   console.log('submitNickname callback')
+  //   setNickname(internalNickname)
+  // }, [internalNickname, setNickname]);
+
+  useEffect(async () => {
+    async function setGameUuidFromQuery() {
      console.log('setGameUuidFromQuery');
      console.log(location);
      const qsObj = querystring.parse(location.search.substr(1))
@@ -17,14 +49,30 @@ const GamePage = ({location}) => {
      setUuid(qsObj.id)
     }
 
-    setGameUuidFromQuery();
+    await setGameUuidFromQuery();
+    setupNewPeer();
+  }, []);
+
+  const setupNewPeer = useCallback(() => {
+    function setupNewPeerId() {
+      console.log('setupNewPeerId');
+      const peer = new Peer(); 
+      console.log({peer});
+      setPeerId(peer)
+    }
+
+    setupNewPeerId();
   }, []);
 
   return (
     <Layout>
-      <SEO title="GAme Page" />
-      <h1>Hi from the game page</h1>
-      <p>Welcome to the game page for {uuid}</p>
+      <SEO title="Game Page" />
+      <h1>Game Room</h1>
+      { !uuid && <Container>Loading...⏳</Container>}
+      { uuid && !nickname &&  <NicknameInput setNickname={setNickname}/>}
+      {nickname && <p>Welcome to the game room, {nickname}!</p>}
+      {uuid && <p>game id: {uuid}</p>}
+
       <Link to="/">Go back to the homepage</Link>
     </Layout>
   )
